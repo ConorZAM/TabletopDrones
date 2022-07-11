f8c6806cfc4274af13877c67ba18d5391896fd36
ConorZAM <32569138+ConorZAM@users.noreply.github.com>
2022-07-11 15:28:29 +0100

    main upload

---
+++ WayPoint Tools/Scripts/KinematicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicController : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float rotationSpeed;
+
+    // We could have this in Update or FixedUpdate, doesn't really matter
+    void Update()
+    {
+        // Move forwards at constant speed
+        transform.position += Time.deltaTime * speed * transform.forward;
+
+        // Rotate towards the target
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+}
